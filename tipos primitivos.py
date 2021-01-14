291428595035581f604ad899cd671a1507d69717
Thiago <65466044+thiagobezerra2@users.noreply.github.com>
2021-01-13 23:12:37 -0300

    Add files via upload

---
+++ tipos primitivos.py
@@ -0,0 +1,8 @@
+n1 = input('Digite algo:')
+print('O tipo primitivo desse valor é', type(n1))
+print('Só tem espaços', n1.isspace())
+print('É um número?', n1.isnumeric())
+print('É alfabético?', n1.isalpha())
+print('É alfanumerico?', n1.isalnum())
+print('Está me maiúsculo?', n1.islower())
+print('Esta em minusculo?', n1.isupper())
